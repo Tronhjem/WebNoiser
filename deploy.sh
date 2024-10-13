f50063e70c007ef6aed600174b473f2a620fb01e
Chris_Server <ctronhjem@gmail.com>
2024-10-13 16:26:51 +0000

    deploy is server only now

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
-./build.sh
+echo Removing /root/dev/WebNoiser/dist/
+rm -r /root/dev/WebNoiser/dist/
+echo Running NPM build... 
+echo ======== web pack start ===============
+(cd /root/dev/WebNoiser/ ; npm run build)
+echo Remove /var/www/noisenook.net/html/*
 rm -r /var/www/noisenook.net/html/*
-cp -r ./dist/* /var/www/noisenook.net/html/
+echo ======= web pack end ================
+echo Copy to /var/www/noisenook.net/html/
+cp -r /root/dev/WebNoiser/dist/* /var/www/noisenook.net/html/
 
